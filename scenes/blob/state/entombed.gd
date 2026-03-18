9b334e82f1c1b4df0659c70b952839db302f845e
Evan Johnson <johnsonev207@gmail.com>
2026-03-18 08:22:59 -0700

    Add particles on lizard statue damage + destroy

--- scenes/blob/state/entombed.gd
+++ scenes/blob/state/entombed.gd
@@ -6,13 +6,6 @@ const SPAWN_IMPULSE_MAGNITUDE: float = 800.0
 var _hp: int = 2
 var _damage_cooldown: bool = false
 
-@onready var sound_effect_config_damage: SoundEffectConfig = preload(
-	"res://scenes/blob/sound_effects/statue_damage.tres"
-)
-@onready var sound_effect_config_emerge: SoundEffectConfig = preload(
-	"res://scenes/blob/sound_effects/blob_emerge.tres"
-)
-
 
 # If not on cooldown, further damage the statue on any key press/mouse click
 func handle_input(event: InputEvent) -> void:
@@ -24,15 +17,20 @@ func handle_input(event: InputEvent) -> void:
 
 	if _hp > 0:
 		_hp -= 1
-		push_warning("TODO: Spawn some particles for statue damage.")
 		blob.sprite_statue.region_rect.position.x += 32
-		SoundEffectManager.play_effect_at(sound_effect_config_damage, blob.global_position)
+		SoundEffectManager.play_effect_at(_get_damage_sound_effect(), blob.global_position)
+
+		var damage_particles: GPUParticles2D = _get_damage_particles()
+		damage_particles.global_position = blob.global_position + Vector2(0, -10)
+		damage_particles.emitting = true
+		LevelManager.current_level.add_child(damage_particles)
+		damage_particles.finished.connect(damage_particles.queue_free)
+
 		_damage_cooldown = true
 		get_tree().create_timer(DAMAGE_COOLDOWN_DURATION).timeout.connect(
 			func() -> void: _damage_cooldown = false
 		)
 	else:
-		push_warning("TODO: Spawn some particles for statue destroy.")
 		state_machine.transition_to("Airborne")
 		blob.animation_player.play("jump_up")
 
@@ -47,8 +45,35 @@ func exit() -> void:
 	blob.freeze = false
 	# Give the appearance that the blob is spawning from the center of the statue
 	blob.position.y -= 16.0
+
+	var lizard_statue_fragments: GPUParticles2D = _get_lizard_statue_fragments()
+	lizard_statue_fragments.global_position = blob.global_position
+	lizard_statue_fragments.emitting = true
+	LevelManager.current_level.add_child(lizard_statue_fragments)
+	lizard_statue_fragments.finished.connect(lizard_statue_fragments.queue_free)
+
 	blob.sprite_statue.visible = false
 	blob.sprite.visible = true
 	blob.animation_player.play("jump_up")
-	SoundEffectManager.play_effect_at(sound_effect_config_emerge, blob.global_position)
+	SoundEffectManager.play_effect_at(_get_emerge_sound_effect(), blob.global_position)
 	blob.apply_central_impulse(Vector2.UP * SPAWN_IMPULSE_MAGNITUDE)
+
+
+func _get_damage_particles() -> GPUParticles2D:
+	var packed_scene: PackedScene = load("res://scenes/blob/lizard_statue_damage_particles.tscn")
+	return packed_scene.instantiate()
+
+
+func _get_lizard_statue_fragments() -> GPUParticles2D:
+	var packed_scene: PackedScene = load(
+		"res://scenes/blob/lizard_statue_fragments/lizard_statue_fragments.tscn"
+	)
+	return packed_scene.instantiate()
+
+
+func _get_damage_sound_effect() -> SoundEffectConfig:
+	return load("res://scenes/blob/sound_effects/statue_damage.tres")
+
+
+func _get_emerge_sound_effect() -> SoundEffectConfig:
+	return load("res://scenes/blob/sound_effects/blob_emerge.tres")
